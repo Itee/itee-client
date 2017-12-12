983e730dd52712e4bf68423c41a9e820f48b22be
IG\tvalcke <tvalcke@geomap-imagis.com>
2017-12-12 21:39:24 +0100

    Add default sources file

---
+++ sources/itee-client.js
@@ -0,0 +1,29 @@
+/**
+ * @author Tristan Valcke <valcketristan@gmail.com>
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ * @see https://github.com/Itee
+ *
+ * @namespace T
+ *
+ * @file The main entry point for Itee-Client, it contains all exports of the library
+ *
+ */
+
+/**
+ * Class representing the main application
+ *
+ * @class
+ */
+class Application {
+
+    /**
+     * Method that alert an hello world
+     *
+     * @returns {HTML}
+     */
+    start () {
+        alert('Hello World !')
+    }
+}
+
+export { Application }
