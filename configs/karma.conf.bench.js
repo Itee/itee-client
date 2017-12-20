c5ec7dfa94a533066e5a6dab99462301472a02d1
IG\tvalcke <tvalcke@geomap-imagis.com>
2017-12-20 09:48:09 +0100

    style

--- configs/karma.conf.bench.js
+++ configs/karma.conf.bench.js
@@ -10,61 +10,70 @@
 
 /* eslint-env node */
 
-module.exports = function (config) {
-  config.set({
+/**
+ * Will assign an appropriate configuration object for karma.
+ *
+ * @param {object} config - The karma configuration object to extend
+ */
+function CreateKarmaBenchmarkConfiguration( config ) {
+
+    config.set( {
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../',
+        basePath: '../',
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [ 'benchmark' ],
+        frameworks: [ 'benchmark' ],
 
         // list of files / patterns to load in the browser
-    files: [
-      'tests/benchmarks/**/*.bench.js'
-    ],
+        files: [
+            'tests/benchmarks/**/*.bench.js'
+        ],
 
         // list of files to exclude
-    exclude: [],
+        exclude: [],
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+        preprocessors: {},
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        reporters: [ 'progress' ],
 
         // web server port
-    port: 9876,
+        port: 9876,
 
         // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        colors: true,
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        logLevel: config.LOG_INFO,
 
         // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+        autoWatch: false,
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [ 'Chrome' ],
+        browsers: [ 'Chrome' ],
         //        browsers: ['Chrome', 'Firefox', 'Safari', 'IE'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+        singleRun: true,
 
         // Concurrency level
         // how many browser should be started simultaneous
-    concurrency: 1,
+        concurrency: 1,
 
         // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000
+        captureTimeout: 60000
 
-  })
+    } )
+
 }
+
+module.exports = CreateKarmaBenchmarkConfiguration
