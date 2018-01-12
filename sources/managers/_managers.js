b4b946fee281a44893ccfb52301bad9b60a9d70b
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 11:18:35 +0100

    Add intermediary export files in managers folder

---
+++ sources/managers/_managers.js
@@ -0,0 +1,11 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Intermediary export file for managers folder
+ *
+ */
+
+export { TDataBaseManager } from './TDataBaseManager'
+export { TErrorManager } from './TErrorManager'
+export { TProgressManager } from './TProgressManager'
