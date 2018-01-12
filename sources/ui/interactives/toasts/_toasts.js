fe207aebc31b6bd5773a6b17f3cca2a7fd0abd08
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 11:33:17 +0100

    Add intermediary export files in ui/interactive/toasts folder

---
+++ sources/ui/interactives/toasts/_toasts.js
@@ -0,0 +1,15 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Intermediary export file for ui/interactives/toasts folder
+ *
+ */
+
+export { TErrorToast } from './TErrorToast'
+export { TInfoToast } from './TInfoToast'
+export { TMessageToast } from './TMessageToast'
+export { TMessageToast } from './TMessageToast'
+export { TProgressToast } from './TProgressToast'
+export { TToast } from './TToast'
+export { TWarningToast } from './TWarningToast'
