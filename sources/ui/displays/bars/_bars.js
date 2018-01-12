b24326fada20a3e4c45a2829726a2c3d6a9a3c10
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 11:20:11 +0100

    Add intermediary export files in ui/bars folder

---
+++ sources/ui/displays/bars/_bars.js
@@ -0,0 +1,12 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Intermediary export file for ui/displays/bars folder
+ *
+ */
+
+export { TAppBar } from './TAppBar'
+export { TStatusBar } from './TStatusBar'
+export { TTitleBar } from './TTitleBar'
+export { TToolBar } from './TToolBar'
