9f53a739a5ceee5a5f72e8c350249b298558244b
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 11:19:16 +0100

    Add intermediary export files in ui folder

---
+++ sources/ui/_ui.js
@@ -0,0 +1,12 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Intermediary export file for ui folder
+ *
+ */
+
+export * from './displays/_displays'
+export * from './inputs/_inputs'
+export * from './interactives/_interactives'
+export * from './templates/_templates'
