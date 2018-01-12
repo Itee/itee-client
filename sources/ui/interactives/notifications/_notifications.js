f999a30be5f4c629dcef7d4b65da66ee1721b849
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 11:32:59 +0100

    Add intermediary export files in ui/interactive/notifications folder

---
+++ sources/ui/interactives/notifications/_notifications.js
@@ -0,0 +1,14 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Intermediary export file for ui/interactives/notifications folder
+ *
+ */
+
+export { TErrorNotification } from './TErrorNotification'
+export { TInfoNotification } from './TInfoNotification'
+export { TMessageNotification } from './TMessageNotification'
+export { TNotification } from './TNotification'
+export { TProgressNotification } from './TProgressNotification'
+export { TWarningNotification } from './TWarningNotification'
