931e4725e53f432ff3a4d8ed7eac5f6e386caf7f
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 19:12:23 +0100

    TDateTime: Implementation

--- sources/ui/displays/bases/TDateTime.js
+++ sources/ui/displays/bases/TDateTime.js
@@ -2,33 +2,66 @@
  * @author [Tristan Valcke]{@link https://github.com/Itee}
  * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
  *
- * @file Todo
+ * @class TDateTime
+ * @classdesc The TDateTime is design to do...
  *
- * @example Todo
+ * @example
+ *
+ * <TDateTime></TDateTime>
  *
  */
 
 import React from 'react'
+import PropTypes from 'prop-types'
 
 let _instanceCounter = 0
 
 class TDateTime extends React.Component {
+
+    static defaultProps = {
+        refreshInterval: 1000
+    }
 
     constructor ( props ) {
 
         super( props )
         _instanceCounter++
 
+        this.state = {
+            date: new Date()
+        }
+
+        this._timerID = undefined
+
+        // Handlers binding
+
     }
 
     /**
-     * React lifecycle
+     * Component Life Cycle
      */
+    static componentDidCatch ( error, info ) {
+
+        console.error( error )
+
+    }
+
     componentWillMount () {}
 
-    componentDidMount () {}
+    componentDidMount () {
 
-    componentWillUnmount () {}
+        this._timerID = setInterval(
+            () => this.tick(),
+            this.props.refreshInterval
+        )
+
+    }
+
+    componentWillUnmount () {
+
+        clearInterval( this._timerID )
+
+    }
 
     componentWillReceiveProps ( nextProps ) {}
 
@@ -38,26 +71,50 @@ class TDateTime extends React.Component {
 
     componentDidUpdate ( prevProps, prevState ) {}
 
-    static componentDidCatch ( error, info ) {
 
-        console.error( error )
+    /**
+     * Component Handlers
+     */
+
+
+    /**
+     * Component Methods
+     */
+    tick () {
+
+        this.setState( {
+            date: new Date()
+        } )
 
     }
+
+    /**
+     * Component Rendering
+     */
 
     render () {
 
         const { id, className } = this.props
 
         const _id = id || `tDateTime_${_instanceCounter}`
-        const _style = {}
         const _class = ( className ) ? `tDateTime ${className}` : 'tDateTime'
 
+        const _style = {}
+
         return (
-            <t-date-time ref={( container ) => {this._container = container}} id={_id} style={_style} className={_class}></t-date-time>
+            <t-date-time id={_id} style={_style} className={_class} />
         )
 
     }
 
+}
+
+/**
+ * Class props type checking
+ */
+TDateTime.propType = {
+    id: PropTypes.string,
+    className: PropTypes.string
 }
 
 export { TDateTime }
