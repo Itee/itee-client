9130e4bf85ee50b6bbcb1500c025718839b1e833
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 11:27:52 +0100

    Add intermediary export files in ui/inputs/buttons folder

---
+++ sources/ui/inputs/buttons/_buttons.js
@@ -0,0 +1,14 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Intermediary export file for ui/inputs/buttons folder
+ *
+ */
+
+export { TButton } from './TButton'
+export { TButtonGroup } from './TButtonGroup'
+export { TLogingButton } from './TLoginButton'
+export { TRadioButton } from './TRadioButton'
+export { TSortButton } from './TSortButton'
+export { TToolButton } from './TToolButton'
