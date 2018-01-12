9ba7ef0c13cd3a7d6852826f1092137119dd8a4a
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 11:22:28 +0100

    Add intermediary export files in ui/displays/tabs folder

---
+++ sources/ui/displays/tabs/_tabs.js
@@ -0,0 +1,10 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Intermediary export file for ui/displays/tabs folder
+ *
+ */
+
+export { TTab } from './TTab'
+export { TTabLayout } from './TTabLayout'
