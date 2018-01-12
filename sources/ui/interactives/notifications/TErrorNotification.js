32fa834362d35c37792527bf59d05a7565f24ea6
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 11:34:29 +0100

    Add missings react template implementation in TErrorNotification and TWarningNotification

--- sources/ui/interactives/notifications/TErrorNotification.js
+++ sources/ui/interactives/notifications/TErrorNotification.js
@@ -8,3 +8,56 @@
  *
  */
 
+import React from 'react'
+
+let _instanceCounter = 0
+
+class TErrorNotification extends React.Component {
+
+    constructor ( props ) {
+
+        super( props )
+        _instanceCounter++
+
+    }
+
+    /**
+     * React lifecycle
+     */
+    componentWillMount () {}
+
+    componentDidMount () {}
+
+    componentWillUnmount () {}
+
+    componentWillReceiveProps ( nextProps ) {}
+
+    shouldComponentUpdate ( nextProps, nextState ) {}
+
+    componentWillUpdate ( nextProps, nextState ) {}
+
+    componentDidUpdate ( prevProps, prevState ) {}
+
+    static componentDidCatch ( error, info ) {
+
+        console.error( error )
+
+    }
+
+    render () {
+
+        const { id, className } = this.props
+
+        const _id = id || `tErrorNotification_${_instanceCounter}`
+        const _style = {}
+        const _class = ( className ) ? `tErrorNotification ${className}` : 'tErrorNotification'
+
+        return (
+            <t-error-notification ref={( container ) => {this._container = container}} id={_id} style={_style} className={_class}></t-error-notification>
+        )
+
+    }
+
+}
+
+export { TErrorNotification }
--- sources/ui/interactives/notifications/TWarningNotification.js
+++ sources/ui/interactives/notifications/TWarningNotification.js
@@ -8,3 +8,57 @@
  *
  */
 
+import React from 'react'
+
+let _instanceCounter = 0
+
+class TWarningNotification extends React.Component {
+
+    constructor ( props ) {
+
+        super( props )
+        _instanceCounter++
+
+    }
+
+    /**
+     * React lifecycle
+     */
+    componentWillMount () {}
+
+    componentDidMount () {}
+
+    componentWillUnmount () {}
+
+    componentWillReceiveProps ( nextProps ) {}
+
+    shouldComponentUpdate ( nextProps, nextState ) {}
+
+    componentWillUpdate ( nextProps, nextState ) {}
+
+    componentDidUpdate ( prevProps, prevState ) {}
+
+    static componentDidCatch ( error, info ) {
+
+        console.error( error )
+
+    }
+
+    render () {
+
+        const { id, className } = this.props
+
+        const _id = id || `tWarningNotification_${_instanceCounter}`
+        const _style = {}
+        const _class = ( className ) ? `tWarningNotification ${className}` : 'tWarningNotification'
+
+        return (
+            <t-warning-notification ref={( container ) => {this._container = container}} id={_id} style={_style} className={_class}></t-warning-notification>
+        )
+
+    }
+
+}
+
+export { TWarningNotification }
+
