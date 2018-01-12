312f5a7b40ec8893dd6b38b6b39b9b2c99542e4e
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 11:21:53 +0100

    Add intermediary export files in ui/displays/sections folder

---
+++ sources/ui/displays/sections/_sections.js
@@ -0,0 +1,14 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Intermediary export file for ui/displays/sections folder
+ *
+ */
+
+export { TContent } from './TContent'
+export { TFooter } from './TFooter'
+export { THeader } from './THeader'
+export { TInfinitScroll } from './TInfinitScroll'
+export { TSection } from './TSection'
+export { TSection } from './TSection'
