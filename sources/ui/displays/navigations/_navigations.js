48c44e6131cfed75692f6e317bb4dc7aebdb6db6
Tristan VALCKE <valcketristan@gmail.com>
2018-01-12 11:21:40 +0100

    Add intermediary export files in ui/displays/navigations folder

---
+++ sources/ui/displays/navigations/_navigations.js
@@ -0,0 +1,20 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Intermediary export file for ui/displays/navigations folder
+ *
+ */
+
+export { TAffix } from './TAffix'
+export { TAnchor } from './TAnchor'
+export { TBackToTop } from './TBackToTop'
+export { TBrand } from './TBrand'
+export { TBreadcrumb } from './TBreadcrumb'
+export { TDropDownMenu } from './TDropDownMenu'
+export { TMenu } from './TMenu'
+export { TMenuItem } from './TMenuItem'
+export { TNavbar } from './TNavbar'
+export { TPagination } from './TPagination'
+export { TSideBar } from './TSideBar'
+export { TStepper } from './TStepper'
