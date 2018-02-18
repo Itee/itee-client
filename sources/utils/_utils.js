80d9f30cf1d000f650a5038c6e77cd9b2c4621e6
Tristan VALCKE <valcketristan@gmail.com>
2018-02-18 16:31:23 +0100

    Add intermediary exporter files

---
+++ sources/utils/_utils.js
@@ -0,0 +1,14 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Todo
+ *
+ * @example Todo
+ *
+ */
+
+export * from './TObjectUtil'
+export * from './TStringUtil'
+export * from './TTemperatureUtil'
+export * from './TTrigonometricUtil'
