bd87db01add4243cda946cb408cc40088d652401
Tristan VALCKE <valcketristan@gmail.com>
2018-02-18 10:54:28 +0100

    TCube: Start normalization

--- sources/objects3d/TCube.js
+++ sources/objects3d/TCube.js
@@ -4,79 +4,79 @@
 
 /* eslint-env browser */
 
-(function ( $ ) {
-    'use strict';
+import {
+    BoxGeometry,
+    MeshBasicMaterial,
+    FaceColors,
+    Mesh
+} from 'threejs-full-es6'
 
-    // Pre-required
-    if ( typeof NODIX.ui === 'undefined' && NODIX.debug ) {
-        throw new Error( 'NODIX.ui need to be define before NODIX.ui.Cube, sorry for the disagreement...' );
+import { extend } from '../utils/TUtils'
+
+function Cube ( givenSettings ) {
+
+    var _ = this;
+
+    _.settings = extend( Cube.DEFAULT_SETTINGS, givenSettings );
+
+    _.geometry = new BoxGeometry( _.settings.width, _.settings.height, _.settings.depth, _.settings.widthSegments, _.settings.heightSegments, _.settings.depthSegments );
+
+    var color = Math.random() * 0xffffff;
+    for ( var i = 0 ; i < _.geometry.faces.length ; i++ ) {
+        if ( 0 === (i % 2) ) {
+            color = Math.random() * 0xffffff;
+        }
+        _.geometry.faces[ i ].color.setHex( color );
     }
+    _.material = new MeshBasicMaterial( {
+        color:        0xffffff,
+        vertexColors: FaceColors
+    } );
 
-    var _ui = NODIX.ui;
+    _.position = _.settings.position;
 
-    _ui.Cube = _ui.Cube || function Cube ( givenSettings ) {
+    _.view = new Mesh( _.geometry, _.material );
+    _.view.position.set( _.position.x, _.position.y, _.position.z );
+    _.view.castShadow    = true;
+    _.view.receiveShadow = false;
 
-        var _ = this;
+    return _;
 
-        _.settings = $.extend( {}, _ui.Cube.DEFAULT_SETTINGS, givenSettings );
+}
 
-        _.geometry = new THREE.BoxGeometry( _.settings.width, _.settings.height, _.settings.depth, _.settings.widthSegments, _.settings.heightSegments, _.settings.depthSegments );
+Cube.DEFAULT_SETTINGS = {
+    width:          1.0,
+    height:         1.0,
+    depth:          1.0,
+    widthSegments:  1.0,
+    heightSegments: 1.0,
+    depthSegments:  1.0,
+    position:       {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0
+    }
+}
 
-        var color = Math.random() * 0xffffff;
-        for ( var i = 0 ; i < _.geometry.faces.length ; i++ ) {
-            if ( 0 === (i % 2) ) {
-                color = Math.random() * 0xffffff;
-            }
-            _.geometry.faces[ i ].color.setHex( color );
-        }
-        _.material = new THREE.MeshBasicMaterial( {
-            color:        0xffffff,
-            vertexColors: THREE.FaceColors
-        } );
+Cube.prototype.setPosition = function ( x, y, z ) {
 
-        _.position = _.settings.position;
+    var _ = this;
+    _.view.position.set( x, y, z );
 
-        _.view = new THREE.Mesh( _.geometry, _.material );
-        _.view.position.set( _.position.x, _.position.y, _.position.z );
-        _.view.castShadow    = true;
-        _.view.receiveShadow = false;
+};
 
-        return _;
+Cube.prototype.update = function () {
+    var _ = this;
+    _.autoRotate();
+};
 
-    };
+Cube.prototype.autoRotate = function () {
 
-    _ui.Cube.DEFAULT_SETTINGS = {
-        width:          1.0,
-        height:         1.0,
-        depth:          1.0,
-        widthSegments:  1.0,
-        heightSegments: 1.0,
-        depthSegments:  1.0,
-        position:       {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0
-        }
-    };
+    var _ = this;
 
-    _ui.Cube.prototype.setPosition = function ( x, y, z ) {
+    _.view.rotation.x += 0.005;
+    _.view.rotation.y += 0.005;
+};
 
-        var _ = this;
-        _.view.position.set( x, y, z );
+export { Cube }
 
-    };
-
-    _ui.Cube.prototype.update = function () {
-        var _ = this;
-        _.autoRotate();
-    };
-
-    _ui.Cube.prototype.autoRotate = function () {
-
-        var _ = this;
-
-        _.view.rotation.x += 0.005;
-        _.view.rotation.y += 0.005;
-    };
-
-})( jQuery );
