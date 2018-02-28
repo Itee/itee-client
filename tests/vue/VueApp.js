a238304d7de647913fae9f69e748a4d52a42dfc4
Tristan VALCKE <valcketristan@gmail.com>
2018-02-28 12:46:28 +0100

    Add Test about Vue

---
+++ tests/vue/VueApp.js
@@ -0,0 +1,17 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Todo
+ *
+ * @example Todo
+ *
+ */
+
+
+import './TContainer'
+import './TLabel'
+import './TItemList'
+import './TButton'
+
+export { default as createSpaceCraft } from '../../node_modules/vue/dist/vue.esm'
