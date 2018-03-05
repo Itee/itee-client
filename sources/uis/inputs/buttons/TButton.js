9c3dbd7445adac15a43b860e96292015d7657fb1
Tristan VALCKE <valcketristan@gmail.com>
2018-03-05 09:17:22 +0100

    TButton: Convert to vue

--- sources/uis/inputs/buttons/TButton.js
+++ sources/uis/inputs/buttons/TButton.js
@@ -8,66 +8,12 @@
  *
  */
 
-/* eslint-env browser */
+import Vue from '../../../node_modules/vue/dist/vue.esm'
 
-import React from 'react'
+export default Vue.component( 'TButton', {
+    template: `
+        <button class="button" @click="onClick(messageData)"><i v-if="icon" className={icon}></i>{{label}}</button>
+    `,
+    props:    [ 'label', 'onClick', 'messageData' ]
+} )
 
-let _instanceCounter = 0
-
-class TButton extends React.Component {
-
-    render () {
-        const { id, label, icon, onClickHandler } = this.props
-
-        const _id    = id || `tButton_${_instanceCounter}`
-
-        if ( label && icon ) {
-
-            return (
-                <button
-                    id={_id}
-                    className="btn"
-                    onClick={onClickHandler}>
-                    <i className={icon}></i>
-                    {label}
-                </button>
-            )
-
-        } else if ( label && !icon ) {
-
-            return (
-                <button
-                    id={_id}
-                    className="btn"
-                    onClick={onClickHandler}>
-                    {label}
-                </button>
-            )
-
-        } else if ( !label && icon ) {
-
-            return (
-                <button
-                    id={_id}
-                    className="btn"
-                    onClick={onClickHandler}>
-                    <i className={icon}></i>
-                </button>
-            )
-
-        } else {
-
-            return (
-                <button
-                    id={_id}
-                    className="btn"
-                    onClick={onClickHandler}>
-                </button>
-            )
-
-        }
-    }
-
-}
-
-export { TButton }
