35031d3f9883366548c7da3e84af3ad33eaa5a9d
Tristan VALCKE <valcketristan@gmail.com>
2018-03-05 09:12:34 +0100

    TViewport3D: convert to vue

--- sources/uis/displays/medias/TViewport3D.js
+++ sources/uis/displays/medias/TViewport3D.js
@@ -10,96 +10,141 @@
 
 /* eslint-env browser */
 
-import React from 'react'
-import {
-    WebGLRenderer,
-    Scene,
-    PerspectiveCamera,
-    Vector3,
-    AmbientLight,
-    BoxBufferGeometry,
-    MeshPhongMaterial,
-    Mesh,
-    GridHelper,
-    OrbitControls
-} from 'threejs-full-es6'
+import { MeshPhongMaterial } from '../../../../node_modules/threejs-full-es6/sources/materials/MeshPhongMaterial'
+import { PerspectiveCamera } from '../../../../node_modules/threejs-full-es6/sources/cameras/PerspectiveCamera'
+import { WebGLRenderer } from '../../../../node_modules/threejs-full-es6/sources/renderers/WebGLRenderer'
+import { Scene } from '../../../../node_modules/threejs-full-es6/sources/scenes/Scene'
+import { Vector3 } from '../../../../node_modules/threejs-full-es6/sources/math/Vector3'
+import { AmbientLight } from '../../../../node_modules/threejs-full-es6/sources/lights/AmbientLight'
+import { Mesh } from '../../../../node_modules/threejs-full-es6/sources/objects/Mesh'
+import { GridHelper } from '../../../../node_modules/threejs-full-es6/sources/helpers/GridHelper'
+import { OrbitControls } from '../../../../node_modules/threejs-full-es6/sources/controls/OrbitControls'
+import { BoxBufferGeometry } from '../../../../node_modules/threejs-full-es6/sources/geometries/BoxGeometry'
 
-let _instanceCounter = 0
+import Vue from '../../../../node_modules/vue/dist/vue.esm'
+import resize from 'vue-resize-directive'
 
-class TViewport3D extends React.Component {
+import { DefaultLogger as TLogger } from '../../../loggers/TLogger'
+import { isString } from '../../../validators/TStringValidator'
 
-    constructor ( props ) {
+export default Vue.component( 'TViewport3D', {
+    template:   `
+        <div class="tViewport3D" v-resize:debounce="_resize"></div>
+    `,
+    directives: {
+        resize
+    },
+    data:       function () {
 
-        super( props )
-        _instanceCounter++
+        return {
+            _frameId:      undefined,
+            _renderer:     new WebGLRenderer( { antialias: true } ),
+            _scene:        new Scene(),
+            _camera:       new PerspectiveCamera(),
+            _orbitControl: undefined,
+            _cube:         undefined,
+            _needResize:   '',
+        }
 
-        this._container    = undefined
-        this._frameId      = undefined
-        this._renderer     = new WebGLRenderer( { antialias: true } )
-        this._scene        = new Scene()
-        this._camera       = new PerspectiveCamera()
-        this._orbitControl = undefined
-        this._cube         = undefined
+    },
+    props:      [ 'width', 'height', 'cameraType', 'currentEffect', 'currentMode' ],
+    methods:    {
 
-        // Handler
-        this._resize = this._resize.bind( this )
+        _startLoop () {
 
-    }
+            if ( this.$data._frameId ) {
+                return
+            }
 
-    /**
-     * React lifecycle
-     */
-    componentWillMount () {
+            this.$data._frameId = window.requestAnimationFrame( this._loop.bind( this ) )
+
+        },
+
+        _loop () {
+
+            this.$data._frameId = window.requestAnimationFrame( this._loop.bind( this ) )
+
+            // Perform loop work here
+            const SPEED = 0.01
+            this.$data._cube.rotation.x -= SPEED * 2
+            this.$data._cube.rotation.y -= SPEED
+            this.$data._cube.rotation.z -= SPEED * 3
+
+            this.$data._orbitControl.update()
+
+            this.$data._renderer.render( this.$data._scene, this.$data._camera )
+
+        },
+
+        _stopLoop () {
+
+            window.cancelAnimationFrame( this.$data._frameId )
+
+        },
+
+        _resize ( domElement ) {
+
+            const data            = this.$data
+            const containerWidth  = domElement.offsetWidth
+            const containerHeight = domElement.offsetHeight || 1 // In case height === 0 set to 1
+
+            data._renderer.setSize( containerWidth, containerHeight )
+            data._camera.aspect = ( containerWidth / containerHeight )
+            data._camera.updateProjectionMatrix()
+
+            console.log( `TViewport3D._resize(w/h): ${containerWidth}/${containerHeight}` )
+
+        }
+
+    },
+    created () {
 
         window.addEventListener( 'resize', this._resize, false )
 
-    }
+    },
+    mounted () {
 
-    componentDidMount () {
-
-        this._resize()
+        const domElement = this.$el
+        const data       = this.$data
 
         // Set renderer
-        this._renderer.setClearColor( 0x777777 )
-        this._renderer.autoClear = true
+        data._renderer.setClearColor( 0x777777 )
+        data._renderer.autoClear = true
 
         // Add renderer canvas
-        this._container.appendChild( this._renderer.domElement )
+        domElement.appendChild( data._renderer.domElement )
 
         // Set camera position
-        this._camera.fov        = 50
-        this._camera.near       = 0.01
-        this._camera.far        = 10000
-        this._camera.position.x = 0.0
-        this._camera.position.y = 5.0
-        this._camera.position.z = 7.0
-        this._camera.setRotationFromAxisAngle( new Vector3( 1.0, 0.0, 0.0 ), -0.610865 )
-        this._camera.updateProjectionMatrix()
+        data._camera.fov        = 50
+        data._camera.near       = 0.01
+        data._camera.far        = 10000
+        data._camera.position.x = 0.0
+        data._camera.position.y = 5.0
+        data._camera.position.z = 7.0
+        data._camera.setRotationFromAxisAngle( new Vector3( 1.0, 0.0, 0.0 ), -0.610865 )
+        data._camera.updateProjectionMatrix()
 
         // Init camera controls
-        this._orbitControl             = new OrbitControls( this._camera, this._container )
-        this._orbitControl.maxDistance = 2000
+        data._orbitControl             = new OrbitControls( data._camera, domElement )
+        data._orbitControl.maxDistance = 2000
 
         // Add light
-        this._scene.add( new AmbientLight( 0xC8C8C8 ) )
+        data._scene.add( new AmbientLight( 0xC8C8C8 ) )
 
         // Create the scene
         const geometry = new BoxBufferGeometry( 1, 1, 1 )
         const material = new MeshPhongMaterial( 0x0096FF )
-        this._cube     = new Mesh( geometry, material )
-        this._scene.add( this._cube )
+        data._cube     = new Mesh( geometry, material )
+        data._scene.add( data._cube )
 
         const gridHelper = new GridHelper( 100, 100 )
-        this._scene.add( gridHelper )
-
-        // Add listener
+        data._scene.add( gridHelper )
 
         // Start rendering
         this._startLoop()
 
-    }
-
-    componentWillUnmount () {
+    },
+    beforeDestroy () {
 
         this._stopLoop()
 
@@ -107,79 +152,4 @@ class TViewport3D extends React.Component {
 
     }
 
-    componentWillReceiveProps ( /*nextProps*/ ) {}
-
-    //shouldComponentUpdate ( /*nextProps, nextState*/ ) {}
-
-    componentWillUpdate ( /*nextProps, nextState*/ ) {}
-
-    componentDidUpdate ( /*prevProps, prevState*/ ) {}
-
-    render () {
-
-        const { id, className } = this.props
-
-        const _id    = id || `tViewport3D_${_instanceCounter}`
-        const _style = {
-            width:    '100%',
-            height:   '100%',
-            overflow: 'hidden',
-            display:  'block'
-        }
-        const _class = ( className ) ? `tViewport3D ${className}` : 'tViewport3D'
-
-        return (
-            <t-viewport-3d ref={( viewport ) => { this._container = viewport }} id={_id} style={_style} class={_class}></t-viewport-3d>
-        )
-
-    }
-
-    /**
-     * Component methods
-     */
-    _startLoop () {
-
-        if ( this._frameId ) {
-            return
-        }
-
-        this._frameId = window.requestAnimationFrame( this._loop.bind( this ) )
-
-    }
-
-    _loop () {
-
-        this._frameId = window.requestAnimationFrame( this._loop.bind( this ) )
-
-        // Perform loop work here
-        const SPEED = 0.01
-        this._cube.rotation.x -= SPEED * 2
-        this._cube.rotation.y -= SPEED
-        this._cube.rotation.z -= SPEED * 3
-
-        this._orbitControl.update()
-
-        this._renderer.render( this._scene, this._camera )
-
-    }
-
-    _stopLoop () {
-
-        window.cancelAnimationFrame( this._frameId )
-
-    }
-
-    _resize () {
-
-        const containerWidth  = this._container.clientWidth
-        const containerHeight = this._container.clientHeight || 1 // In case height === 0 set to 1
-
-        this._renderer.setSize( containerWidth, containerHeight )
-        this._camera.aspect = ( containerWidth / containerHeight )
-        this._camera.updateProjectionMatrix()
-
-    }
-
-}
-
-export { TViewport3D }
+} )
