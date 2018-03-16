3fba81f5dd2a4e967a053fd757de1dc6022b64cc
Tristan VALCKE <valcketristan@gmail.com>
2018-03-16 22:14:06 +0100

    IteeClient: import fontawesome icons

--- sources/itee-client.js
+++ sources/itee-client.js
@@ -12,6 +12,10 @@
 import './third_party/dock-spawn.js'
 import './third_party/polyfills.js'
 
+// Import FontAwesome
+import '@fortawesome/fontawesome-free-solid'
+import '@fortawesome/fontawesome-free-brands'
+
 // Export es6 Three stuff
 export * from 'threejs-full-es6'
 
