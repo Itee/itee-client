3fef300065d8ffe7b56db66ce89310aa52097558
Tristan VALCKE <valcketristan@gmail.com>
2018-08-23 16:39:51 +0200

    TIcon: Fix fontawesome breakage

--- sources/itee-client.js
+++ sources/itee-client.js
@@ -14,6 +14,7 @@ import './third_party/polyfills.js'
 import 'babel-polyfill'
 
 // Import FontAwesome
+import '@fortawesome/fontawesome'
 import '@fortawesome/fontawesome-free-solid'
 import '@fortawesome/fontawesome-free-brands'
 
