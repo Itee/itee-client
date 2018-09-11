87d51b3e24945bba2d40344d9b0b5bef38f23ccb
Tristan VALCKE <valcketristan@gmail.com>
2018-09-11 11:08:02 +0200

    TOrbitControlsHelper: Remove impose, dispose and control. This need to be managed in the controller itself

--- sources/objects3d/TOrbitControlsHelper.js
+++ sources/objects3d/TOrbitControlsHelper.js
@@ -28,37 +28,37 @@ import {
 
 class TOrbitControlsHelper extends LineSegments {
 
-    constructor ( control, radius = 2, radials = 16, circles = 2, divisions = 64, innerColor = new Color( 0x444444 ), outerColor = new Color( 0x888888 ) ) {
+    constructor ( /*control,*/ radius = 2, radials = 16, circles = 2, divisions = 64, innerColor = new Color( 0x444444 ), outerColor = new Color( 0x888888 ) ) {
         super( TOrbitControlsHelper._createInternalGeometry( radius, radials, circles, divisions, innerColor, outerColor ), TOrbitControlsHelper._createInternalMaterial() )
 
-        this.control     = control
+//        this.control     = control
         this._intervalId = undefined
 
-        this.impose()
+//        this.impose()
 
     }
 
-    get control () {
-
-        return this._control
-
-    }
-
-    set control ( value ) {
-
-        if ( isNull( value ) ) { throw new Error( 'Control cannot be null ! Expect an instace of camera controller.' ) }
-        if ( isUndefined( value ) ) { throw new Error( 'Control cannot be undefined ! Expect an instace of camera controller.' ) }
-
-        this._control = value
-
-    }
-
-    setControl ( value ) {
-
-        this.control = value
-        return this
-
-    }
+//    get control () {
+//
+//        return this._control
+//
+//    }
+//
+//    set control ( value ) {
+//
+//        if ( isNull( value ) ) { throw new Error( 'Control cannot be null ! Expect an instace of camera controller.' ) }
+//        if ( isUndefined( value ) ) { throw new Error( 'Control cannot be undefined ! Expect an instace of camera controller.' ) }
+//
+//        this._control = value
+//
+//    }
+//
+//    setControl ( value ) {
+//
+//        this.control = value
+//        return this
+//
+//    }
 
     static _createInternalGeometry ( RADIUS, RADIALS, CIRCLES, DIVISIONS, color1, color2 ) {
 
@@ -154,34 +154,34 @@ class TOrbitControlsHelper extends LineSegments {
 
     }
 
-    impose () {
+//    impose () {
+//
+//        this._control.addEventListener( 'start', this.startOpacityAnimation.bind( this ) )
+//        this._control.addEventListener( 'change', this.updateHelperPosition.bind( this ) )
+//        this._control.addEventListener( 'end', this.endOpacityAnimation.bind( this ) )
+//
+//    }
 
-        this._control.addEventListener( 'start', this.startOpacityAnimation.bind( this ) )
-        this._control.addEventListener( 'change', this.updateHelperPosition.bind( this ) )
-        this._control.addEventListener( 'end', this.endOpacityAnimation.bind( this ) )
-
-    }
-
-    dispose () {
-
-        this._control.removeEventListener( 'start', this.startOpacityAnimation )
-        this._control.removeEventListener( 'change', this.updateHelperPosition )
-        this._control.removeEventListener( 'end', this.endOpacityAnimation )
-
-    }
+//    dispose () {
+//
+//        this._control.removeEventListener( 'start', this.startOpacityAnimation )
+//        this._control.removeEventListener( 'change', this.updateHelperPosition )
+//        this._control.removeEventListener( 'end', this.endOpacityAnimation )
+//
+//    }
 
     /**
      *
      */
-    updateHelperPosition () {
-
-        const target = this._control.target
-
-        this.position.x = target.x
-        this.position.y = target.y
-        this.position.z = target.z
-
-    }
+//    updateHelperPosition () {
+//
+//        const target = this._control.target
+//
+//        this.position.x = target.x
+//        this.position.y = target.y
+//        this.position.z = target.z
+//
+//    }
 
     /**
      *
@@ -225,193 +225,5 @@ class TOrbitControlsHelper extends LineSegments {
     }
 
 }
-
-//
-///**
-// *
-// * @param orbitControls
-// * @constructor
-// */
-//function TOrbitControlsHelper ( orbitControls ) {
-//
-//    if ( !orbitControls ) { throw new Error( 'Unable to create TOrbitControlsHelper for null or undefined controller !' ) }
-//    if ( !orbitControls instanceof OrbitControls ) { throw new Error( 'Parameter need to be an OrbitControls !' ) }
-//
-//    var RADIUS    = 2
-//    var RADIALS   = 16
-//    var CIRCLES   = 2
-//    var DIVISIONS = 64
-//    var color1    = new Color( 0x444444 )
-//    var color2    = new Color( 0x888888 )
-//
-//    var vertices = []
-//    var colors   = []
-//
-//    var x, z;
-//    var v, i, j, r, color;
-//
-//    // create the radials
-//    for ( i = 0 ; i <= RADIALS ; i++ ) {
-//
-//        v = ( i / RADIALS ) * ( Math.PI * 2 );
-//
-//        x = Math.sin( v ) * RADIUS;
-//        z = Math.cos( v ) * RADIUS;
-//
-//        vertices.push( 0, 0, 0 );
-//        vertices.push( x, 0, z );
-//
-//        color = ( i & 1 ) ? color1 : color2;
-//
-//        colors.push( color.r, color.g, color.b );
-//        colors.push( color.r, color.g, color.b );
-//
-//    }
-//
-//    // create the circles
-//    for ( i = 0 ; i <= CIRCLES ; i++ ) {
-//
-//        color = ( i & 1 ) ? color1 : color2;
-//
-//        r = RADIUS - ( RADIUS / CIRCLES * i );
-//
-//        for ( j = 0 ; j < DIVISIONS ; j++ ) {
-//
-//            // first vertex
-//            v = ( j / DIVISIONS ) * ( Math.PI * 2 );
-//
-//            x = Math.sin( v ) * r;
-//            z = Math.cos( v ) * r;
-//
-//            vertices.push( x, 0, z );
-//            colors.push( color.r, color.g, color.b );
-//
-//            // second vertex
-//            v = ( ( j + 1 ) / DIVISIONS ) * ( Math.PI * 2 );
-//
-//            x = Math.sin( v ) * r;
-//            z = Math.cos( v ) * r;
-//
-//            vertices.push( x, 0, z );
-//            colors.push( color.r, color.g, color.b );
-//
-//        }
-//
-//        // create axis
-//        vertices.push(
-//            -1, 0, 0, 1, 0, 0,
-//            0, -1, 0, 0, 1, 0,
-//            0, 0, -1, 0, 0, 1
-//        )
-//        colors.push(
-//            1, 0, 0, 1, 0.6, 0,
-//            0, 1, 0, 0.6, 1, 0,
-//            0, 0, 1, 0, 0.6, 1
-//        )
-//
-//    }
-//
-//    const positionBufferAttribute = new Float32BufferAttribute( vertices, 3 )
-//    positionBufferAttribute.name  = 'TOrbitControlsHelperPositionBufferAttribute'
-//
-//    const colorBufferAttribute = new Float32BufferAttribute( colors, 3 )
-//    colorBufferAttribute.name  = 'TOrbitControlsHelperColorBufferAttribute'
-//
-//    const geometry = new BufferGeometry()
-//    geometry.addAttribute( 'position', positionBufferAttribute )
-//    geometry.addAttribute( 'color', colorBufferAttribute )
-//    geometry.name = 'TOrbitControlsHelperGeometry'
-//
-//    const material       = new LineBasicMaterial( { vertexColors: VertexColors } )
-//    material.transparent = true
-//    material.opacity     = 0.0
-//    material.name        = 'TOrbitControlsHelperMaterial'
-//
-//    LineSegments.call( this, geometry, material )
-//
-//    this.control    = orbitControls
-//    this.intervalId = undefined
-//    this.impose()
-//
-//}
-//
-//TOrbitControlsHelper.prototype = Object.assign( Object.create( LineSegments.prototype ), {
-//
-//    /**
-//     *
-//     */
-//    constructor: TOrbitControlsHelper,
-//
-//    impose () {
-//
-//        this.control.addEventListener( 'start', this.startOpacityAnimation.bind( this ) )
-//        this.control.addEventListener( 'change', this.updateHelperPosition.bind( this ) )
-//        this.control.addEventListener( 'end', this.endOpacityAnimation.bind( this ) )
-//
-//    },
-//
-//    dispose () {
-//
-//        this.control.removeEventListener( 'start', this.startOpacityAnimation )
-//        this.control.removeEventListener( 'change', this.updateHelperPosition )
-//        this.control.removeEventListener( 'end', this.endOpacityAnimation )
-//
-//    },
-//
-//    /**
-//     *
-//     */
-//    updateHelperPosition () {
-//
-//        const target = this.control.target
-//
-//        this.position.x = target.x
-//        this.position.y = target.y
-//        this.position.z = target.z
-//
-//    },
-//
-//    /**
-//     *
-//     */
-//    startOpacityAnimation () {
-//
-//        // In case fade off is running, kill it an restore opacity to 1
-//        if ( this.intervalId !== undefined ) {
-//
-//            clearInterval( this.intervalId )
-//            this.intervalId = undefined
-//
-//        }
-//
-//        this.material.opacity = 1.0
-//
-//    },
-//
-//    /**
-//     *
-//     */
-//    endOpacityAnimation () {
-//
-//        // Manage transparency interval
-//        this.intervalId = setInterval( function () {
-//
-//            if ( this.material.opacity <= 0.0 ) {
-//
-//                this.material.opacity = 0.0
-//                clearInterval( this.intervalId )
-//                this.intervalId = undefined
-//
-//            } else {
-//
-//                this.material.opacity -= 0.1
-//
-//            }
-//
-//        }.bind( this ), 100 )
-//
-//    }
-//
-//} )
 
 export { TOrbitControlsHelper }
