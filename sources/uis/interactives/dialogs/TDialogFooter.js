5556ee738a635eccd5fe1c87691d0f42ae39592c
Tristan VALCKE <valcketristan@gmail.com>
2019-01-04 11:12:55 +0100

    TDialogFooter: Make safer props and use idFactory

--- sources/uis/interactives/dialogs/TDialogFooter.js
+++ sources/uis/interactives/dialogs/TDialogFooter.js
@@ -11,12 +11,27 @@
 /* eslint-env browser */
 
 import Vue from '../../../../node_modules/vue/dist/vue.esm'
+import { TIdFactory, TIdFactoryType } from '../../../utils/TIdFactory'
+
+const IdFactory = new TIdFactory( TIdFactoryType.String, 't-dialog-header-' )
 
 export default Vue.component( 'TDialogFooter', {
     template: `
-        <div class="modal-footer">
+        <div :id="id" class="modal-footer">
             <slot></slot>
+            <button v-if="haveCloseButton" type="button" class="close" data-dismiss="modal" aria-label="Close" v-on:click="$emit('close')">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
     `,
-    props:    [ 'id', 'title' ]
+    props:    {
+        id:         {
+            type:    String,
+            default: IdFactory.createId()
+        },
+        haveCloseButton: {
+            type:    Boolean,
+            default: false
+        }
+    }
 } )
