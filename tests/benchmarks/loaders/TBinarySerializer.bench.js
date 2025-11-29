929beb29bd7b679431bb7f4361a69ae6bc6a7d58
Tristan VALCKE <valcketristan@gmail.com>
2025-11-29 17:24:59 +0100

    :recycle: (benchmarks) regenerate

--- tests/benchmarks/loaders/TBinarySerializer.bench.js
+++ tests/benchmarks/loaders/TBinarySerializer.bench.js
@@ -1,5 +1,9 @@
-import { Testing }      from 'itee-utils/sources/testings/benchmarks.js'
 import * as TBinarySerializerNamespace from '../../../sources/loaders/TBinarySerializer.js'
+import { getBenchmarkPackage } from '../../import.benchmarks.js'
+import { getTestingPackage } from '../../import.testing.js'
+
+const Benchmark = await getBenchmarkPackage()
+const Testing   = await getTestingPackage()
 
 const isDateSuite = Benchmark.Suite( 'TBinarySerializerNamespace.isDate', Testing.createSuiteOptions() )
                                      .add( 'isDate()', Testing.iterateOverDataMap( TBinarySerializerNamespace.isDate ), Testing.createBenchmarkOptions() )
